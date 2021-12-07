5f0cd59f66b6651e23c48305e7f6be057d953302
s1128571 <s1128571@student.hsleiden.nl>
2021-12-07 22:46:22 +0100

    add basic styling
    this is the first verion of the basic styling of the front page. only stlyling and basic logic is applyed

---
+++ src/app/afkoteek/dropdown/dropdown.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-dropdown',
+  templateUrl: './dropdown.component.html',
+  styleUrls: ['./dropdown.component.scss']
+})
+export class DropdownComponent implements OnInit {
+  // organisatieForm = this.form.group();
+
+  dropdownItem = [
+    'item',
+    'iets anders',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed',
+    'rijksbreed'
+  ];
+
+
+  constructor(private form: FormBuilder) { }
+
+  organisatieForm = this.form.group({
+    placeholder: 'test',
+    organisatie: [null]
+  });
+
+  ngOnInit(): void {
+
+  }
+
+  submit() {
+    console.log(this.organisatieForm.value)
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {AfkoteekComponent} from "./afkoteek/afkoteek.component";
+import {ResultComponent} from "./afkoteek/result/result.component";
+import {FooterComponent} from "./footer/footer.component";
+import {DropdownComponent} from "./afkoteek/dropdown/dropdown.component";
+import {SearchComponent} from "./afkoteek/search/search.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AfkoteekComponent,
+    SearchComponent,
+    ResultComponent,
+    FooterComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
