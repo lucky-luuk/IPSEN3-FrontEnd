bae56b444c0db0eb8a958717a3769d952674127b
Roeland195 <47328692+Roeland195@users.noreply.github.com>
2021-12-10 14:44:38 +0100

    Re-work gamePage
    Loading van page gereworked

---
+++ src/app/afkoteek/game/game-page/game-page-start/game-page-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-page-start',
+  templateUrl: './game-page-start.component.html',
+  styleUrls: ['./game-page-start.component.scss']
+})
+export class GamePageStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
