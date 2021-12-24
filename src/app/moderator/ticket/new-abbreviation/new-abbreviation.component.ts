1a72b7bbeb172dd792555a43a466382e91eeb2c7
mees Barnhoorn <meesbarnhoorn@gmail.com>
2021-12-24 20:58:37 +0100

    correct organisation is now selected for on add_abbreviation ticket

--- src/app/moderator/ticket/new-abbreviation/new-abbreviation.component.ts
+++ src/app/moderator/ticket/new-abbreviation/new-abbreviation.component.ts
@@ -1,20 +1,26 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
 import {AbbreviationModel} from "../../../afkoteek/search/abbreviation-list/abbreviation.model";
 import {AbbreviationService} from "../../../afkoteek/search/abbreviation-list/abbreviation.service";
+import {DropdownComponent} from "../../../afkoteek/search/dropdown/dropdown.component";
 
 @Component({
   selector: 'app-new-abbreviation',
   templateUrl: './new-abbreviation.component.html',
   styleUrls: ['./new-abbreviation.component.scss']
 })
-export class NewAbbreviationComponent implements OnInit {
- @Input() abbrModel: AbbreviationModel;
+export class NewAbbreviationComponent implements OnInit, AfterViewInit {
+  @Input() abbrModel: AbbreviationModel;
+  @ViewChild(DropdownComponent) dropdown : any;
 
-  constructor(private abbrService: AbbreviationService) {
+  constructor() {
    this.abbrModel = new AbbreviationModel();
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() : void {
+    this.dropdown.selectOrganisation(this.abbrModel.organisations[0]);
   }
 
 }
