dd386c9c119cf801d6723234b09ded51b8b9f311
s1128571 <s1128571@student.hsleiden.nl>
2022-01-12 10:58:34 +0100

    quick fix admin

--- src/app/admin/edit-mod/edit-mod.component.ts
+++ src/app/admin/edit-mod/edit-mod.component.ts
@@ -72,12 +72,12 @@ export class EditModComponent implements OnInit {
 
   updateUser(f : any) {
     let data: { firstName: string, lastName: string, email: string } = f.form.value
-    let user = new UsersModel();
-    user.firstName = data.firstName;
-    user.lastName = data.lastName;
-    user.email = data.email
+    // let user = new UsersModel();
+    this.user.firstName = data.firstName;
+    this.user.lastName = data.lastName;
+    this.user.email = data.email;
     // user.phoneNumber = data.phoneNumber;
-    this.usersService.updateUsers(user, () => {
+    this.usersService.updateUsers(this.user, () => {
     });
   }
 
