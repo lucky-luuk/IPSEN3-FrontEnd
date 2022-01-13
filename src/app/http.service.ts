36a43a53c10559a73ae15232e2a3d7fd36b4c60a
meesmb <60484127+meesmb@users.noreply.github.com>
2022-01-13 17:21:24 +0100

    added hashing and loginService

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -7,6 +7,7 @@ import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
 })
 export class HttpService {
   public static readonly RESPONSE_SUCCESS_CODE = "SUCCESS";
+  public static readonly RESPONSE_FAILURE_CODE = "FAILURE";
 
   private url : string = "http://localhost:8080";
   private http : HttpClient;
@@ -16,29 +17,34 @@ export class HttpService {
   }
 
   // data is null if api call returns no data!!!!
-  public get<T>(endpoint : string, args : Map<string, string>, implementation : (data : T) => void) {
+  public get<T>(endpoint : string, args : Map<string, string>, implementation : (data : T) => void, onFailure : () => void = () => {}) {
     endpoint = this.getEndpointWithArguments(endpoint, args);
 
     this.http.get<HttpResponse<T>>(this.url + endpoint).subscribe((response) => {
-      HttpService.callImplementation<T>(response, implementation);
+      HttpService.callImplementation<T>(response, implementation, onFailure);
     });
   }
 
-  public post<T>(endpoint : string, body : T, implementation : (data : T) => void) {
+  public post<T>(endpoint : string, body : T, implementation : (data : T) => void, onFailure : () => void = () => {}) {
     this.http.post<HttpResponse<T>>(this.url + endpoint, body).subscribe((response) => {
-      HttpService.callImplementation<T>(response, implementation);
+      HttpService.callImplementation<T>(response, implementation, onFailure);
+    });
+  }
+  public postWithReturnType<T, R>(endpoint : string, body : T, implementation : (data : R) => void, onFailure : () => void = () => {}) {
+    this.http.post<HttpResponse<R>>(this.url + endpoint, body, ).subscribe((response) => {
+      HttpService.callImplementation<R>(response, implementation, onFailure);
     });
   }
 
-  public put<T>(endpoint : string, body : T, implementation : (data : T) => void) {
+  public put<T>(endpoint : string, body : T, implementation : (data : T) => void, onFailure : () => void = () => {}) {
     this.http.put<HttpResponse<T>>(this.url + endpoint, body).subscribe((response) => {
-      HttpService.callImplementation<T>(response, implementation);
+      HttpService.callImplementation<T>(response, implementation, onFailure);
     });
   }
 
-  public delete<T>(endpoint : string, body : T, implementation : (data : T) => void) {
+  public delete<T>(endpoint : string, body : T, implementation : (data : T) => void, onFailure : () => void = () => {}) {
     this.http.delete<HttpResponse<T>>(this.url + endpoint, {body: body}).subscribe((response) => {
-      HttpService.callImplementation<T>(response, implementation);
+      HttpService.callImplementation<T>(response, implementation, onFailure);
     });
   }
 
@@ -53,14 +59,17 @@ export class HttpService {
     return endpoint;
   }
 
-  private static callImplementation<T>(response : HttpResponse<T>, implementation : (data : T) => void | null) : void {
+  private static callImplementation<T>(response : HttpResponse<T>, implementation : (data : T) => void | null, onFailure : () => void) : void {
     // if error
-    if (response.response !== HttpService.RESPONSE_SUCCESS_CODE) {
+    if (response.response === HttpService.RESPONSE_FAILURE_CODE) {
       HttpService.onError(response.error);
+      onFailure();
     }
-    implementation(response.data);
+    else {
+      if (implementation !== null)
+        implementation(response.data);
+    }
   }
-
   private static onError(message : string) {
     console.log(message);
   }
---
+++ src/app/login.service.ts
@@ -0,0 +1,37 @@
+import {Injectable} from '@angular/core';
+import {Md5} from "ts-md5";
+import {HttpService} from "./http.service";
+import {AccountModel} from "./account.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http : HttpService) { }
+
+  login(email: string, password: string, onSuccess: (data: {email: string, firstname: string, lastname: string, token: string}) => void, onFailure: () => void) {
+    let hash = Md5.hashStr(password);
+    this.http.postWithReturnType <{username: string, password: string}, {email: string, firstname: string, lastname: string, token: string}>(
+      "/authenticate", {username: email, password: hash}, onSuccess, onFailure);
+  }
+
+  createAccount(account: AccountModel, onSuccess: (data: {id: string, firstName: string, lastName: string, email: string, roles: {name: string}[]}) => void, onFailure: () => void) {
+    // create new account so we dont change the model
+    let a = this.copyAccount(account);
+    a.password = Md5.hashStr(account.password);
+
+    this.http.postWithReturnType<AccountModel, {id: string, firstName: string, lastName: string, email: string, roles: {name: string}[]}>(
+      "/register", a, onSuccess, onFailure);
+  }
+
+  private copyAccount(a : AccountModel) : AccountModel {
+    let a2 = new AccountModel();
+    a2.firstName = a.firstName;
+    a2.password = a.password;
+    a2.id = a.id;
+    a2.email = a.email;
+    a2.lastName = a.lastName;
+    return a2;
+  }
+}
