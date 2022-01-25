848fbde45ed0a0eceadb090fe8e2b4ffd366f117
Roeland195 <47328692+Roeland195@users.noreply.github.com>
2022-01-25 23:54:10 +0100

    password reseting

--- src/app/auth/first-login-popup/first-login-popup.component.ts
+++ src/app/auth/first-login-popup/first-login-popup.component.ts
@@ -3,6 +3,9 @@ import {AbbreviationModel} from "../../afkoteek/search/abbreviation-list/abbrevi
 import {ModalDismissReasons, NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {AbbreviationService} from "../../afkoteek/search/abbreviation-list/abbreviation.service";
 import {FormControl, FormGroup} from "@angular/forms";
+import { HttpService } from 'src/app/http.service';
+import { LoginService } from '../login.service';
+import { Md5 } from 'ts-md5';
 
 @Component({
   selector: 'app-first-login-popup',
@@ -13,22 +16,24 @@ export class FirstLoginPopupComponent implements OnInit {
   passwordSame: boolean = true;
   form = new FormGroup({
     password : new FormControl(),
-    password2 : new FormControl()
+    password2 : new FormControl(),
+    newpassword: new FormControl()
   });
   closeResult = '';
 
   onClose = ()=>{}
 
   abbreviation = new AbbreviationModel();
-  constructor(public activeModal: NgbActiveModal, private abbreviationService: AbbreviationService) {}
+  constructor(public activeModal: NgbActiveModal, private loginservice: LoginService) {}
 
 
   submit(){
-    let password = this.form.get("password")?.value;
-    let password2 = this.form.get("password2")?.value;
-    if (password === password2){
-      this.passwordSame = true;
-
+    let oldpassword: string = this.form.get("password")?.value;
+    let oldpasswordtwo: string = this.form.get("password2")?.value;
+    let newpassword = this.form.get("newpassword")?.value;
+    if (oldpassword === oldpasswordtwo){
+     let email = JSON.stringify(localStorage.getItem('userEmail'));
+      this.loginservice.resetPassword({email, oldpassword, newpassword});
     } else{
       this.passwordSame = false;
     }
--- src/app/auth/login.service.ts
+++ src/app/auth/login.service.ts
@@ -24,6 +24,7 @@ export class LoginService {
         if (data.firstLogin){
           console.log("in if")
           this.modal.open(FirstLoginPopupComponent);
+          localStorage.setItem('userEmail', data.email);
         }
         this.handleLogin(data.token);
         if (this.role === 'ADMIN') {
@@ -68,6 +69,12 @@ export class LoginService {
       return !jwtHelper.isTokenExpired(token);
     }
     return false;
+  }
+
+  public resetPassword(changePasswordRequestBody : {email: string, oldpassword: string, newpassword: string}) {
+    this.http.put<{email: string, oldpassword: string, newpassword: string}>("/account/mod/password", changePasswordRequestBody, (data) => {
+    });
+
   }
 
   private handleLogin(token: any) {
