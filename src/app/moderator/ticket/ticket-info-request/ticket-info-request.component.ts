d71fc957b8fa594ae79b3c4b1249bc98cc99a988
mees Barnhoorn <meesbarnhoorn@gmail.com>
2022-01-26 16:00:48 +0100

    set hover and cursor on answer, set abbreviation get limit to 100, make start game button gray when disabled, fix button disable logic, made behandelaar in ticket.component.ts a dropdown, made ticket type display user friendly, changed info request to open user default email client

--- src/app/moderator/ticket/ticket-info-request/ticket-info-request.component.ts
+++ src/app/moderator/ticket/ticket-info-request/ticket-info-request.component.ts
@@ -15,4 +15,13 @@ export class TicketInfoRequestComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  openDefaultEmailClient() {
+    let addresses = this.ticket.userEmail;//between the speech mark goes the receptient. Seperate addresses with a ;
+    let body = ""//write the message text between the speech marks or put a variable in the place of the speech marks
+    let subject = "Afkorijk informatie verzoek"//between the speech marks goes the subject of the message
+    let href = "mailto:" + addresses + "?"
+      + "subject=" + subject + "&"
+      + "body=" + body;
+    window.open(href, "_blank", "scrollbars=yes,resizable=yes,width=300,height=300");
+  }
 }
