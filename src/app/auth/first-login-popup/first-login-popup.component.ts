a86270f21bcff92d9103fbb36068fe21408741f4
Roeland195 <47328692+Roeland195@users.noreply.github.com>
2022-01-26 13:13:03 +0100

    Create first-login-popup.component.ts

---
+++ src/app/auth/first-login-popup/first-login-popup.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {AbbreviationModel} from "../../afkoteek/search/abbreviation-list/abbreviation.model";
+import {ModalDismissReasons, NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {AbbreviationService} from "../../afkoteek/search/abbreviation-list/abbreviation.service";
+import {FormControl, FormGroup} from "@angular/forms";
+import { Md5 } from 'ts-md5';
+import { LoginService } from '../login.service';
+
+@Component({
+  selector: 'app-first-login-popup',
+  templateUrl: './first-login-popup.component.html',
+  styleUrls: ['./first-login-popup.component.scss']
+})
+export class FirstLoginPopupComponent implements OnInit {
+  passwordSame: boolean = true;
+  form = new FormGroup({
+    password : new FormControl(),
+    newpassword: new FormControl(),
+    newpasswordtwo : new FormControl()
+  });
+  closeResult = '';
+
+  onClose = ()=>{}
+
+  abbreviation = new AbbreviationModel();
+  constructor(public activeModal: NgbActiveModal, private loginservice: LoginService) {}
+
+
+  submit(){
+    let oldPassword: string = this.form.get("password")?.value;
+    let newPassword = this.form.get("newpassword")?.value;
+    let newpasswordtwo: string = this.form.get("newpasswordtwo")?.value;
+    if (newPassword === newpasswordtwo){
+      newPassword = Md5.hashStr(newPassword);
+      // this.loginservice.resetPassword({oldPassword, newPassword});
+    } else{
+      this.passwordSame = false;
+    }
+  }
+
+  onSave(){
+    this.activeModal.close();
+    this.onClose();
+  }
+  ngOnInit(): void {
+  }
+
+}
